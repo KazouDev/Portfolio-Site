617bc3d6327d74349761945702c37eb676d8523f
KazouDev <50242895+KazouDev@users.noreply.github.com>
2023-04-09 19:37:17 +0000

    updated menu on mobile

--- script.js
+++ script.js
@@ -1,96 +1,51 @@
-var grid = document.querySelector('.grid');
-
-var more_info_buttons = document.querySelectorAll('.buttons');
-
-const delay = ms => new Promise(res => setTimeout(res, ms));
-
-function addButton(div){
-    let buttons = div.querySelector('.buttons');
-    buttons.addEventListener('click', async event =>  {
-        let element = buttons.parentElement;
-        grid.scrollIntoView({behavior: 'smooth', block: 'end'});
-        await delay(900);
-        element.innerHTML = ""
-        element.classList.toggle('hidden');
-    });
-}
-
-var items = document.querySelectorAll('.link-item');
-items.forEach(item => {
-    item.addEventListener('click', event => {
-        let element = document.querySelector('.more-info');
-        element.classList.toggle('hidden');
-        let page = item.getAttribute('data-page');
-        console.log(page)
-        fetch("pages/"+page).then(res => {
-            return res.text();
-        }).then(html => {
-            element.innerHTML = html;
-            addButton(element);
-            element.scrollIntoView({behavior: 'smooth', block: 'start'});
-        });
-    });
-});
-
-
-
-const mute = function(i){
-    i.classList.toggle("fa-volume-mute");
-    i.classList.toggle("fa-volume-up");
-    let previousVideo = i.previousSibling.previousSibling
-    previousVideo.muted = !(previousVideo.muted)
-}
-
-const firstTimeClicked = [];
-
-const videoItems = document.querySelectorAll('.item-video');
-
-videoItems.forEach(element => {
-    element.addEventListener('mouseenter', hoverVideo)
-    element.addEventListener('mouseleave', hideVideo)
-});
-
-function hoverVideo(v){
-    console.log(v.target.children)
-    let video = v.target.children[0];
-    if(!firstTimeClicked.includes(video)){
-        video.muted = true;
-        firstTimeClicked.push(video);
-    }
-    v.target.children[0].play();
-}
-
-function hideVideo(v){
-    v.target.children[0].pause();
-}
-
-function reaveal(){
-    let window_height = window.innerHeight;
-    let elements_reveals = document.querySelectorAll('.reveal');
-    elements_reveals.forEach(element => {
-        if (element.getBoundingClientRect().top + 100 < window_height){
-            element.classList.add('active');
-        } else if (element.getBoundingClientRect().top > window_height){
-            element.classList.remove('active');
+const observerReveal = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if (!entry.target.classList.contains('active')){
+                entry.target.classList.add('active');
+            }
         }
     });
+},
+{
+    threshold: 0.3
 }
+);
 
-function updateNavbar(){
-    let sections = document.querySelectorAll('section');
-    let navButtons = document.querySelectorAll('nav ul li');
-    sections.forEach(section => {
-        let windowTop = window.scrollY;
-        let sectionOffset = section.offsetTop;
-        let sectionHeight = section.offsetHeight;
-        
-    });
-}
+var last_current = null;
 
 let menu = document.querySelector('nav');
+
+const observerCurrent = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if (last_current != null){
+                last_current.classList.toggle('current');
+            }
+            let current = document.querySelector('[section-link='+entry.target.getAttribute('id')+']');
+            console.log('[section-link='+entry.target+']');
+            current.classList.toggle('current');
+            last_current = current;
+        }
+    });
+},
+{
+    threshold: 0.7
+}
+);
+
+
+let elements_reveals = document.querySelectorAll('.reveal');
+elements_reveals.forEach(reveal => {
+    observerReveal.observe(reveal);
+})
+
+let sections = document.querySelectorAll('section');
+sections.forEach(reveal => {
+    observerCurrent.observe(reveal);
+})
+
 let nav_mobile_button = document.querySelector('.mobile-button');
 nav_mobile_button.addEventListener('click', async event => {
     menu.classList.toggle('open');
 });
-
-window.addEventListener('scroll', reaveal);
